d86969bc7b04398f7f3bb2af827dc66b345e1bd7
hfk576@uowmail.edu.au <hfk576@uowmail.edu.au>
2018-03-12 17:46:05 +0800

    Ant in action Ch4

---
+++ Ant in action/ch4/test/com/core/test/SimpleTest.java
@@ -0,0 +1,13 @@
+
+package com.core.test;
+import junit.framework.TestCase;
+
+public class SimpleTest extends TestCase {
+	public SimpleTest(String s) {
+	super(s);
+	}
+
+	public void testCreation() {
+	System.out.println("Hello test");
+	}
+}
